c097515dc17761cc50a80751a99841c4d91a8b0b
shawrylk <shawry.lk@gmail.com>
2019-12-15 11:02:00 +0700

    reorganizing folder structure

--- build.sh
+++ build.sh
@@ -1,14 +1,53 @@
 #!/bin/bash
 
-FOLDER=./Fingerprint/
+HOME=$(pwd)
+SOURCE=src
+INCLUDE=inc
+STUB=stub
 
-trap "echo \"*****Exit program******\"; rm -f Finger;" SIGINT SIGTERM
+trap "echo \"*****Exit program******\"; rm -f $HOME/Finger;" SIGINT SIGTERM
 
-echo "*****Start compiling*****"
-rm -f Finger
-mv $FOLDER/Fingerprint.ino $FOLDER/Fingerprint.cpp
-g++ -o Finger $FOLDER/*.cpp ./stub/*.cpp -I ./stub/ -std=gnu++17 -Wno-narrowing  -Wno-write-strings 
-mv $FOLDER/Fingerprint.cpp $FOLDER/Fingerprint.ino
+setup_stub()
+{
+	echo "*****Setup stub files******"
+	cd $HOME/$STUB
+       	mv $STUB_CPP $HOME/$SOURCE
+       	mv $STUB_H $HOME/$INCLUDE
+	mv $HOME/$SOURCE/Fingerprint.ino $HOME/$SOURCE/Fingerprint.cpp
+}
+
+clean_stub()
+{
+	echo "*****Clean stub files*****"
+	cd $HOME/$SOURCE
+       	mv $STUB_CPP $HOME/$STUB
+	cd $HOME/$INCLUDE
+       	mv $STUB_H $HOME/$STUB
+	mv $HOME/$SOURCE/Fingerprint.cpp $HOME/$SOURCE/Fingerprint.ino
+}
+
+rm -f $HOME/Finger
+
+if [ -d "$STUB" ]
+then
+	STUB_CPP=$(find $HOME/$STUB -name *.cpp -printf "%f ")
+	STUB_H=$(find $HOME/$STUB -name *.h -printf "%f ")
+	setup_stub
+	echo "*****Start compiling*****"
+	g++ -o $HOME/Finger $HOME/$SOURCE/*.cpp -I $HOME/$INCLUDE -std=gnu++17 -Wno-narrowing  -Wno-write-strings
+	if [ $? == 1 ]
+	then
+		echo "Compilation fail, exit script"
+		clean_stub
+		exit 1
+	fi
+	clean_stub
+else
+	echo "Missing stub files, exit script"
+	exit 1
+fi
+
 echo "*****Compile done*****"
 echo "*****Start running program*****"
-./Finger
+$HOME/Finger
+
--- stub/stub.cpp
+++ stub/stub.cpp
@@ -10,13 +10,13 @@ int main()
     setup();
     while (1)
     {
-        printf("Enter mode:\n\t0. Enroll\n\t1.Register new fingerprint\n\t2.Update database\n");
+        printf("Enter mode:\n\t0. Enroll\n\t1. Register new fingerprint\n\t2. Update database\n\tPress Ctrl + C to exit\n");
         std::cin >> mymode;
         if (std::cin.fail())
         {
-            std::cout << "Not regconize mode, try enter again ...\n";
+            std::cout << "Not regconize mode, try input again ...\n";
             std::cin.clear();
-            std::cin.ignore(256,'\n');
+            std::cin.ignore(256, '\n');
             continue;
         }
         switch (mymode)
@@ -25,9 +25,9 @@ int main()
         case 1:
         case 2:
             mode = mymode;
-            break;    
+            break;
         default:
-            std::cout << "Not regconize mode, try enter again ...\n";
+            std::cout << "Not regconize mode, try input again ...\n";
             continue;
         }
         loop();
